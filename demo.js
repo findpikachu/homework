02b46706e156fe1a5eab7a52678d9eee31cb5ca7
gaolei <2503404258@qq.com>
2018-03-15 17:04:42 +0800

    first commit

---
+++ demo.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+var dirname = process.argv[2]
+fs.mkdirSync(dirname)
+process.chdir(dirname)
+fs.mkdirSync("css")
+fs.mkdirSync("js")
+fs.writeFileSync("index.html","<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>\n")
+fs.writeFileSync("css/style.css","h1{color: red;}\n")
+fs.writeFileSync("js/main.js",'var string = "Hello World"\nalert(string)\n')
+process.exit(0)
